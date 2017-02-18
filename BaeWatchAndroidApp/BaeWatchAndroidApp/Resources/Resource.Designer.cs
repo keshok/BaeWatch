90a29ecc97534e4957f66941fa9051da6b66c048
mgill62 <manrajgill62@gmail.com>
2017-02-18 13:24:32 -0500

    close #7

--- BaeWatchAndroidApp/BaeWatchAndroidApp/Resources/Resource.Designer.cs
+++ BaeWatchAndroidApp/BaeWatchAndroidApp/Resources/Resource.Designer.cs
@@ -57,11 +57,87 @@ namespace BaeWatchAndroidApp
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int activity_signup = 2131034112;
+			
+			// aapt resource value: 0x7f050012
+			public const int button = 2131034130;
+			
+			// aapt resource value: 0x7f050013
+			public const int button2 = 2131034131;
+			
+			// aapt resource value: 0x7f050002
+			public const int editText = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int editText2 = 2131034116;
+			
+			// aapt resource value: 0x7f050010
+			public const int radioButton = 2131034128;
+			
+			// aapt resource value: 0x7f050007
+			public const int radioButton2 = 2131034119;
+			
+			// aapt resource value: 0x7f050006
+			public const int radioButton3 = 2131034118;
+			
+			// aapt resource value: 0x7f05000a
+			public const int radioButton4 = 2131034122;
+			
+			// aapt resource value: 0x7f050009
+			public const int radioButton5 = 2131034121;
+			
+			// aapt resource value: 0x7f050008
+			public const int radioButton6 = 2131034120;
+			
+			// aapt resource value: 0x7f05000e
+			public const int radioButton7 = 2131034126;
+			
+			// aapt resource value: 0x7f05000d
+			public const int radioButton8 = 2131034125;
+			
+			// aapt resource value: 0x7f05000f
+			public const int radioButton9 = 2131034127;
+			
+			// aapt resource value: 0x7f050001
+			public const int textView = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int textView2 = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int textView3 = 2131034117;
+			
+			// aapt resource value: 0x7f05000b
+			public const int textView4 = 2131034123;
+			
+			// aapt resource value: 0x7f05000c
+			public const int textView5 = 2131034124;
+			
+			// aapt resource value: 0x7f050011
+			public const int textView6 = 2131034129;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Signup = 2130903041;
 			
 			static Layout()
 			{
